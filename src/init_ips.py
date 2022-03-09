9582f2711978fe7b55b87c8304cdd0cbc0b77262
Ubuntu <ubuntu@ip-172-31-27-195.ec2.internal>
2022-03-09 19:57:35 +0000

    work on python dict builder

--- src/init_ips.py
+++ src/init_ips.py
@@ -3,25 +3,45 @@
 
 import sys
 import os.path
+import ipaddress
+import re
 
+def dict_factory(targets):
+	target_ip_lst = ''
+	target_vpc_lst = ''
+	num_targets = int(len(targets)+1)
+	t_index = list(range(1, num_targets, 4))
+	for plc in t_index:
+		target_ip_lst += str(targets[plc] + "\n")
+
+	v_index = list(range(-1, num_targets, 4))
+	for plc in v_index:
+		target_vpc_lst += str(targets[plc] + "\n")
+
+	#Use two lists to make dict - locate VPC where host is as target VPC
+	#Remove all other IPS outside that VPC
+	#Return dict of valid IPs. Send to wrtie function
 
 def main():
-	print(sys.argv[1])
 	if sys.argv[1] == "0":
 		# We are using AWS IPs
-		if os.path.exists("./priv.txt"):
-			f = open("./priv.txt", "r")
-			ft = f.readline()
-			frmt = ft.split('"')[1::2]
-
-			print(frmt)
-			#HERE
-
+		if os.path.exists("./vars/target_info.txt"):
+			f = open("./vars/target_info.txt", "r")
+			ct = 0
+			for l in f.readlines():
+				if ct == 0:
+					host_ip = l.strip()
+					ct += 1
+				else:
+					instance_ips = l.split("'")[1::2]
+					dict_factory(instance_ips)
 		else:
-			print("Fail1")
+			pass
 
 	else:
 		# We are using NMAP IPs
 		print("fail")
 
+
 main()
+
