41dc72694b0eb60f5e1de895746b7dbb00189826
zda2119 <zach.anthony0@gmail.com>
2022-03-10 08:46:50 -0800

    IP/VPC targets -> ansible hosts file

--- src/init_ips.py
+++ src/init_ips.py
@@ -2,11 +2,13 @@
 # It then formats said IPs and appends them to the Ansible Inventory
 
 import sys
-import os.path
+import os
 import ipaddress
 import re
+import shutil
 
-def dict_factory(targets):
+
+def dict_factory(targets, host_ip):
 	target_ip_lst = []
 	target_vpc_lst = []
 	num_targets = int(len(targets)+1)
@@ -18,22 +20,46 @@ def dict_factory(targets):
 	for plc in v_index:
 		target_vpc_lst.append(targets[plc])
 
-	#Use two lists to make dict - locate VPC where host is as target VPC
-	#Remove all other IPS outside that VPC
-	#Return dict of valid IPs. Send to wrtie function
 	target_index = int(len(targets)/4)+1
-	#print(list(range(1, target_index, 1)))
 
 	ip_dict = dict(zip(target_ip_lst, target_vpc_lst))
-	print(ip_dict)
-	# remove where VPC is different modify file to test
+	
+	for key in ip_dict:
+		if key == host_ip:
+			target_vpc = ip_dict.get(key)
+		else:
+			pass
+	target_factory(ip_dict, target_vpc)
 
+
+def target_factory(target_dict, target_vpc):
+	f_target_list = []
+	for key in target_dict:
+		if target_dict.get(key) == target_vpc:
+			f_target_list.append(key)
+		else:
+			pass
+
+	f_target_list_frmt_str = ''
+	for ip in f_target_list:
+		f_target_list_frmt_str += str(ip + ' ansible_python_interpreter=/usr/bin/python3\n')	
+
+	ans_hosts_f = str("/etc/ansible/hosts")
+	if os.path.exists(ans_hosts_f):
+		os.remove(ans_hosts_f)
+		shutil.copyfile("./vars/hosts", ans_hosts_f)
+		r = open(ans_hosts_f, "a+")
+		r.write("\n[targets]\n")
+		r.write(f_target_list_frmt_str)
+		r.close()
+	else:
+		pass
 
 def main():
 	if sys.argv[1] == "0":
 		# We are using AWS IPs
-		if os.path.exists("./vars/target_info.txt"):
-			f = open("./vars/target_info.txt", "r")
+		if os.path.exists("./vars/target_info.tmp"):
+			f = open("./vars/target_info.tmp", "r")
 			ct = 0
 			for l in f.readlines():
 				if ct == 0:
@@ -41,14 +67,13 @@ def main():
 					ct += 1
 				else:
 					instance_ips = l.split("'")[1::2]
-					dict_factory(instance_ips)
+					dict_factory(instance_ips, host_ip)
 		else:
 			pass
 
 	else:
 		# We are using NMAP IPs
-		print("fail")
+		pass
 
 
 main()
-
