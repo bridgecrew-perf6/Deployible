606726ac7dd614f188b012a507d7acbb43bd9c8a
Ubuntu <ubuntu@ip-172-31-27-195.ec2.internal>
2022-04-14 06:34:28 +0000

    issue with snort_install.yml

--- Snort/snort_install.sh
+++ Snort/snort_install.sh
@@ -1,10 +1,9 @@
 #! /bin/bash
 
 wget https://www.snort.org/downloads/snort/daq-2.0.7.tar.gz
-tar xvzf daq-2.0.7.tar.gz
-                      
+tar xvzf daq-2.0.7.tar.gz                      
 cd daq-2.0.7
-./configure && make && sudo make install
+sudo ./configure && make && sudo make install
 
 cd ..
 
@@ -13,3 +12,5 @@ tar xvzf snort-2.9.19.tar.gz
                       
 cd snort-2.9.19
 ./configure --enable-sourcefire && make && sudo make install
+
+sudo ldconfig
